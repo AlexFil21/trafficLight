5dc203d8c10c9a8f23936493e349d628bec172fc
Alexey <hello@MacBook-Air-Bam.local>
2020-08-21 20:00:27 +0300

    traffic

--- app/src/main/java/com/example/trafficlight/MainActivity.kt
+++ app/src/main/java/com/example/trafficlight/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.trafficlight
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    var redLigth: LinearLayout? = null
+    var greenLigth: LinearLayout? = null
+    var yellowLigth: LinearLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        redLigth = findViewById(R.id.redCircle);
+        yellowLigth = findViewById(R.id.yellowCircle);
+        greenLigth = findViewById(R.id.greenCircle);
+    }
+
+    fun changeToRed (view: View) {
+        redLigth?.setBackgroundResource(R.drawable.red)
+        yellowLigth?.setBackgroundResource(R.drawable.dark_circle)
+        greenLigth?.setBackgroundResource(R.drawable.dark_circle)
+
+    }
+
+    fun changeToYellow (view: View) {
+        yellowLigth?.setBackgroundResource(R.drawable.yellow)
+        redLigth?.setBackgroundResource(R.drawable.dark_circle)
+        greenLigth?.setBackgroundResource(R.drawable.dark_circle)
+
+    }
+
+    fun changeToGreen (view: View) {
+        greenLigth?.setBackgroundResource(R.drawable.green)
+        yellowLigth?.setBackgroundResource(R.drawable.dark_circle)
+        redLigth?.setBackgroundResource(R.drawable.dark_circle)
+
     }
 }

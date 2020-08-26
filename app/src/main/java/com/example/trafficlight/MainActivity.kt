ad733c67a2bcbef710af140c6af0d5e3c6fec4fa
Alexey <hello@MacBook-Air-Bam.local>
2020-08-26 23:00:00 +0300

    new1

--- app/src/main/java/com/example/trafficlight/MainActivity.kt
+++ app/src/main/java/com/example/trafficlight/MainActivity.kt
@@ -1,43 +1,35 @@
 package com.example.trafficlight
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
-    var redLigth: LinearLayout? = null
-    var greenLigth: LinearLayout? = null
-    var yellowLigth: LinearLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        redLigth = findViewById(R.id.redCircle);
-        yellowLigth = findViewById(R.id.yellowCircle);
-        greenLigth = findViewById(R.id.greenCircle);
     }
 
     fun changeToRed (view: View) {
-        redLigth?.setBackgroundResource(R.drawable.red)
-        yellowLigth?.setBackgroundResource(R.drawable.dark_circle)
-        greenLigth?.setBackgroundResource(R.drawable.dark_circle)
+        redCircle.setBackgroundResource(R.drawable.red)
+        yellowCircle?.setBackgroundResource(R.drawable.dark_circle)
+        greenCircle?.setBackgroundResource(R.drawable.dark_circle)
 
     }
 
     fun changeToYellow (view: View) {
-        yellowLigth?.setBackgroundResource(R.drawable.yellow)
-        redLigth?.setBackgroundResource(R.drawable.dark_circle)
-        greenLigth?.setBackgroundResource(R.drawable.dark_circle)
+        yellowCircle?.setBackgroundResource(R.drawable.yellow)
+        redCircle?.setBackgroundResource(R.drawable.dark_circle)
+        greenCircle?.setBackgroundResource(R.drawable.dark_circle)
 
     }
 
     fun changeToGreen (view: View) {
-        greenLigth?.setBackgroundResource(R.drawable.green)
-        yellowLigth?.setBackgroundResource(R.drawable.dark_circle)
-        redLigth?.setBackgroundResource(R.drawable.dark_circle)
+        greenCircle?.setBackgroundResource(R.drawable.green)
+        yellowCircle?.setBackgroundResource(R.drawable.dark_circle)
+        redCircle?.setBackgroundResource(R.drawable.dark_circle)
 
     }
 }

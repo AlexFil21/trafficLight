e6d4eb58c9d91b52b8a0700ea698f3dc0d65906a
Alexey <hello@MacBook-Air-Bam.local>
2020-08-27 00:08:20 +0300

    new4

--- app/src/main/java/com/example/trafficlight/MainActivity.kt
+++ app/src/main/java/com/example/trafficlight/MainActivity.kt
@@ -10,26 +10,31 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        redCircle.isEnabled = false
+        yellowCircle.isEnabled = false
+        greenCircle.isEnabled = false
     }
 
     fun changeToRed (view: View) {
-        redCircle.setBackgroundResource(R.drawable.red)
-        yellowCircle?.setBackgroundResource(R.drawable.dark_circle)
-        greenCircle?.setBackgroundResource(R.drawable.dark_circle)
-
+//        redCircle.setBackgroundResource(R.drawable.red)
+//        yellowCircle?.setBackgroundResource(R.drawable.dark_circle)
+//        greenCircle?.setBackgroundResource(R.drawable.dark_circle)
+        redCircle.isEnabled = true;
+        yellowCircle.isEnabled = false;
+        greenCircle.isEnabled = false;
     }
 
     fun changeToYellow (view: View) {
-        yellowCircle?.setBackgroundResource(R.drawable.yellow)
-        redCircle?.setBackgroundResource(R.drawable.dark_circle)
-        greenCircle?.setBackgroundResource(R.drawable.dark_circle)
+        redCircle.isEnabled = false;
+        yellowCircle.isEnabled = true;
+        greenCircle.isEnabled = false;
 
     }
 
     fun changeToGreen (view: View) {
-        greenCircle?.setBackgroundResource(R.drawable.green)
-        yellowCircle?.setBackgroundResource(R.drawable.dark_circle)
-        redCircle?.setBackgroundResource(R.drawable.dark_circle)
-
+        redCircle.isEnabled = false;
+        yellowCircle.isEnabled = false;
+        greenCircle.isEnabled = true;
     }
 }
